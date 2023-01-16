3e3b32b9e897510f9ecea7f6ccaef1d0184c8d3c
vuongDev0509 <vovanvuong591997@gmail.com>
2023-01-16 12:30:02 +0700

    remove md

--- node_modules/@wordpress/components/build-types/scroll-lock/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A component that will lock scrolling when it is mounted and unlock scrolling when it is unmounted.
- *
- * @return {null} Render nothing.
- */
-export default function ScrollLock(): null;
-//# sourceMappingURL=index.d.ts.map
--- node_modules/@wordpress/components/src/navigator/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as NavigatorProvider } from './navigator-provider';
-export { default as NavigatorScreen } from './navigator-screen';
-export { default as NavigatorButton } from './navigator-button';
-export { default as NavigatorBackButton } from './navigator-back-button';
-export { default as useNavigator } from './use-navigator';
