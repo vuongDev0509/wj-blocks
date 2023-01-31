2d78a668abe9017c0a43b9d77fb88e7cfcca67ea
vuongDev0509 <vovanvuong591997@gmail.com>
2023-01-31 16:38:35 +0700

    update animation for  wj-icon-text block

--- src/blocks/wj-icon-text/components/save.js
+++ src/blocks/wj-icon-text/components/save.js
@@ -6,34 +6,48 @@ const Save = (props) => {
     
     return (
         <div className={['wj-icon-text-block', props.className].join(' ')}>
+            { animate ? 
+                <div className='wj-icon-text-block-inner'
+                    data-aos-duration="1200" 
+                    data-aos="fade-in" 
+                    data-aos-once="false" 
+                    data-aos-delay="500"
+                > 
+                    <a href={link}> 
+                        <div className='wj-icon-text-block-icon'> 
+                            <img src={imgUrl} alt="icon" 
+                                style={{
+                                    maxWidth: `${widthImage}px`,
+                                }} 
+                            />
+                        </div>
+
+                        <div className='wj-icon-text-block-content'> 
+                            <div className='wj-icon-text-block-content__inner'>
+                                <InnerBlocks.Content />
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            : 
             <div className='wj-icon-text-block-inner'> 
                 <a href={link}> 
-                <div className='wj-icon-text-block-icon'> 
-                    { animate ?
+                    <div className='wj-icon-text-block-icon'> 
                         <img src={imgUrl} alt="icon" 
                             style={{
                                 maxWidth: `${widthImage}px`,
                             }} 
-                            data-aos-duration="1200" 
-                            data-aos="fade-in" 
-                            data-aos-once="false" 
-                            data-aos-delay="500"
                         />
-                    :
-                    <img src={imgUrl} alt="icon" 
-                        style={{
-                            maxWidth: `${widthImage}px`,
-                        }} 
-                     />
-                    }
-                </div>
-                <div className='wj-icon-text-block-content'> 
-                    <div className='wj-icon-text-block-content__inner'>
-                        <InnerBlocks.Content />
                     </div>
-                </div>
+
+                    <div className='wj-icon-text-block-content'> 
+                        <div className='wj-icon-text-block-content__inner'>
+                            <InnerBlocks.Content />
+                        </div>
+                    </div>
                 </a>
-            </div>  
+            </div>                                
+            } 
         </div>
 	)
 }
